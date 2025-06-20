e699297b79f73155de9411cb702d46fa8846c813
schancay <schancay@pichincha.com>
2025-06-20 12:03:10 -0500

    perf(Service): Karate
    - Best performance

--- src/test/java/com/pichincha/TestRunner.java
+++ src/test/java/com/pichincha/TestRunner.java
@@ -1,16 +1,36 @@
 package com.pichincha;
 
-import com.intuit.karate.junit5.Karate;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 
 class TestRunner {
-    static {
+
+    @BeforeAll
+    static void beforeAll() {
         System.setProperty("karate.ssl", "true");
     }
 
-    @Karate.Test
-    Karate testMarvelCharactersApi() {
-        return Karate.run("classpath:com/pichincha/features/marvel_characters_api")
-                .tags("~@ignore");
+    @AfterAll
+    static void afterAll() {
     }
+
+    @Test
+    void testMarvelCharactersApi() {
+        // Ejecutar tests en paralelo con 4 threads
+        Results results = Runner.path("classpath:com/pichincha/features")
+                .tags("~@ignore")
+                .threads(4)
+                .parallel(4);
+
+        // Opcional: assert para verificar que no haya fallas
+        if (results.getFailCount() > 0) {
+            throw new RuntimeException("Hay escenarios fallidos: " + results.getFailCount());
+        }
+    }
+
 
 }
